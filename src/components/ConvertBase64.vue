2cc45106ba18716f439ab131a115efa658655de5
kioryu <1218moonlight@gmail.com>
2019-09-23 17:24:29 +0900

    [ADD] ConvertBase64.vue and DownloadBase64.vue

---
+++ src/components/ConvertBase64.vue
@@ -0,0 +1,86 @@
+<template>
+    <v-container id="container">
+        <v-flex >
+            <v-label>Image -> Base64</v-label>
+            <v-file-input accept="image/*" @change="changeImg" prepend-icon="mdi-camera"
+                          small-chips show-size filled></v-file-input>
+            <template v-if="targetImg.src !== '' && targetImg.src !== null">
+                <v-img id="image" :src="targetImg.src" :height="targetImg.height" :width="targetImg.width"/>
+                <v-textarea
+                        :value="targetImg.src"
+                        label="Base64"
+                        disabled
+                        full-width
+                        outlined
+                ></v-textarea>
+                <v-btn @click="copyBase64">Copy</v-btn>
+            </template>
+        </v-flex>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "ConvertBase64",
+        data() {
+            return {
+                targetImg: new Image(),
+            }
+        },
+        methods: {
+            changeImg(e) {
+                if (e === null) {
+                    this.targetImg = new Image();
+                } else {
+                    let reader = new FileReader();
+                    let img = new Image();
+
+                    reader.onload = function (el) {
+                        img.onload = function () {
+                            this.targetImg = img;
+                        }.bind(this);
+
+                        img.src = el.currentTarget.result;
+                        img.title = img.alt = e.name
+                    }.bind(this);
+
+                    reader.readAsDataURL(e);
+                }
+            },
+            copyBase64(){
+                let t = document.createElement("textarea");
+                document.body.appendChild(t);
+                t.value = this.targetImg.src;
+                t.select();
+                document.execCommand('copy');
+                document.body.removeChild(t);
+            },
+        }
+    }
+</script>
+
+<style scoped>
+    #image {
+        max-width: 500px;
+        max-height: 300px;
+        margin-bottom: 20px;
+    }
+
+    #container {
+        border: thick double cadetblue;
+        margin-top: 20px;
+    }
+
+    @media (max-width: 600px) {
+        #image {
+            max-width: 300px;
+            max-height: 100px;
+            margin-bottom: 10px;
+        }
+
+        #container {
+            border: thick double coral;
+            margin-top: 10px;
+        }
+    }
+</style>
---
+++ src/components/DownloadBase64.vue
@@ -0,0 +1,69 @@
+<template>
+    <v-container id="container">
+        <v-flex>
+            <v-label>Download Base64</v-label>
+
+            <v-textarea
+                    @change="change"
+                    label="Base64"
+                    full-width
+                    outlined
+            ></v-textarea>
+            <template v-if="targetImg.src !== '' && targetImg.src !== null">
+                <v-btn @click="downloadImg">Download</v-btn>
+            </template>
+        </v-flex>
+    </v-container>
+</template>
+
+<script>
+    import down from 'downloadjs'
+
+    export default {
+        name: "DownloadBase64",
+        data() {
+            return {
+                targetImg: new Image(),
+            }
+        },
+        methods: {
+            change(e) {
+                let img = new Image();
+                img.src = e;
+                this.targetImg = img
+            },
+            downloadImg() {
+                let srcArr = this.targetImg.src.split(',');
+                let type = srcArr[0];
+                // let base64 = srcArr[1];
+                let base64 = this.targetImg.src.replace(/^data:image\/[^;]+/, 'data:application/octet-stream');
+                let fileName = "image";
+
+                if (type.indexOf("png") !== -1) {
+                    down(base64, fileName+'.png', "image/png")
+                } else if (type.indexOf("jpeg") !== -1 || type.indexOf("jpg") !== -1) {
+                    down(base64, fileName+'.jpg', "image/jpg")
+                } else if (type.indexOf("webp") !== -1) {
+                    down(base64, fileName+'.webp', "image/webp")
+                } else {
+                    console.log(null)
+                }
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+    #container {
+        border: thick double coral;
+        margin-top: 20px;
+    }
+
+    @media (max-width: 600px) {
+        #container {
+            border: thick double cadetblue;
+            margin-top: 10px;
+        }
+    }
+</style>
